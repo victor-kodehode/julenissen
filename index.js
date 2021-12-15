6b87e0eafc89c2fa6afbaf068020d85187600786
Simon-Hes <94447502+Simon-Hes@users.noreply.github.com>
2021-12-15 13:00:15 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -25,3 +25,18 @@ window.addEventListener("click",() =>{
     cursor.classList.add("click")
  }  
 })
+
+/* function openFunction()
+{
+    document.getElementById("menu").style.width="300px";
+    document.getElementById("mainbox").style.marginLeft="300px";
+    document.getElementById("mainbox").innerHTML="Click on Cross Element and Close Menu";
+}
+
+function closeFunction()
+{
+    document.getElementById("menu").style.width="0px";
+    document.getElementById("mainbox").style.marginLeft="0px";
+    document.getElementById("mainbox").innerHTML="&#9776; Open";
+    
+} */
